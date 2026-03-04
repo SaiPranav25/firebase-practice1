a067ee8a3dba2d4a085a1928ae2af8ff84bcdd37
SaiPranav25 <bsaipranav_cse2205d5@mgit.ac.in>
2026-03-05 00:21:26 +0530

    added googlesignin

--- firebase-course/src/components/auth.js
+++ firebase-course/src/components/auth.js
@@ -1,6 +1,6 @@
-import { auth } from "../config/firebase";
+import { auth, googleProvider } from "../config/firebase";
 import { useState } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword , signInWithPopup} from "firebase/auth";
 
 export const Auth = () => {
     const [email, setEmail] = useState("");
@@ -14,12 +14,29 @@ export const Auth = () => {
             console.error(error);
         }
     };
+    const signInWithGoogle = async () => {
+        try {
+            await signInWithPopup(auth, googleProvider);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    // const logout = async () => {
+    //     try {
+    //         await signOut(auth);
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // };
 
     return (
         <div>
-            <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <button onClick={signIn}>Sign In</button>
+            <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} /><br />
+            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} /><br />
+            <button onClick={signIn}>Sign In</button><br />
+            <button onClick={signInWithGoogle}>Google Sign In</button><br />
+            {/* <button onClick={logout}>Logout</button><br /> */}
         </div>
     )
 }
