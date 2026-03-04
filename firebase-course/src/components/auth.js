c540428239b65ae52372e7b8ba55583b22c3e325
SaiPranav25 <bsaipranav_cse2205d5@mgit.ac.in>
2026-03-05 00:15:37 +0530

    email password auth.js

--- firebase-course/src/components/auth.js
+++ firebase-course/src/components/auth.js
@@ -1,9 +1,25 @@
+import { auth } from "../config/firebase";
+import { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
 export const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signIn = async () => {
+        try {
+            await createUserWithEmailAndPassword(auth, email, password);
+            
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <div>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button>Sign In</button>
+            <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={signIn}>Sign In</button>
         </div>
     )
 }
