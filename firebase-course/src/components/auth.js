b52e87dcacdd3e6012de41438be33665c3493836
SaiPranav25 <bsaipranav_cse2205d5@mgit.ac.in>
2026-03-05 00:05:55 +0530

    checking auth.js

---
+++ firebase-course/src/components/auth.js
@@ -0,0 +1,9 @@
+export const Auth = () => {
+    return (
+        <div>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Sign In</button>
+        </div>
+    )
+}
