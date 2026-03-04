b62b31a291d7fbb1bd5ae4d93ecd02f4458b1f85
SaiPranav25 <bsaipranav_cse2205d5@mgit.ac.in>
2026-03-05 00:03:56 +0530

    firebase setup initial

---
+++ firebase-course/src/config/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5xAzpnObo1GJ7rPZR8ZyTrScsm5j9vOU",
+  authDomain: "fir-first-45296.firebaseapp.com",
+  projectId: "fir-first-45296",
+  storageBucket: "fir-first-45296.firebasestorage.app",
+  messagingSenderId: "604887829127",
+  appId: "1:604887829127:web:24950e3bb87a7d429fc622",
+  measurementId: "G-MPBTQRWD6W"
+};
+
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+
